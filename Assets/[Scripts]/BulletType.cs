22273c8f7728e50def56efa0fa43c9b309d7425f
Lukeduke91 <gurneylj@gmail.com>
2022-10-23 19:19:51 -0400

    Added more comments

--- Assets/[Scripts]/BulletType.cs
+++ Assets/[Scripts]/BulletType.cs
@@ -1,6 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 
+/// BulletType.cs
+/// Lucas Gurney 
+/// 101313633
+/// October 2 2022 12:05 PM
+/// This code is used to described what kind of bullet it is
+
 [System.Serializable]
 public enum BulletType 
 {
