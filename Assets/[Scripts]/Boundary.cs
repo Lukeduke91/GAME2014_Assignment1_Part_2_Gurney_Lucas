f6f77908aef74f2355c86e80270c58b535f5dca6
Lukeduke91 <gurneylj@gmail.com>
2022-10-23 20:05:16 -0400

    Added the finale comments

--- Assets/[Scripts]/Boundary.cs
+++ Assets/[Scripts]/Boundary.cs
@@ -1,4 +1,8 @@
-
+/// Boundary.cs
+/// Lucas Gurney 
+/// 101313633
+/// October 2 2022 12:05 PM
+/// This code is used set the minimum and the maximum boundaries for the player, bullet, and enemy
 [System.Serializable]
 public struct Boundary
 {
